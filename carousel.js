af2d06f2d9d8e06a45f9da0609b355bdae6165d1
ratatyq <pash.bickov@mail.ru>
2017-09-07 23:20:09 +0300

    upd libery

--- carousel.js
+++ carousel.js
@@ -1,38 +1,42 @@
 var Sls = (function() {
   // code...
 });
-
+/* Глобальные свойства */
 Sls.prototype._properties = {
   NameCarousel: null,
+  StyleCarousel: null,
   WidthSlide: null,
 }
-/*
- * ElemAppend - Выбрать где будет элемент
-*/
+
+/**
+ * Создает карусель
+ * @param {string} ElemAppend Выбрать где будет распологаться элемент
+ * @param {string} idElem Выбрать id для карусели
+ * @param {number} width Указать ширину
+ * @param {number} height Указать высоту
+ */
 Sls.prototype.createCarousel = function(ElemAppend, idElem, width = null, height = null) {
   Slider._properties.NameCarousel = idElem;
   if(typeof ElemAppend === "string") {
     ElemAppend = document.getElementById(ElemAppend);
   }
-  // Создать карусель =>
-  var divCarousel = document.createElement("div");
+  Slider._properties.StyleCarousel = getComputedStyle(ElemAppend);
 
+  var divCarousel = document.createElement("div"); // Главный div карусели
   divCarousel.id = Slider._properties.NameCarousel;
 
   if(width !== null && height !== null) {
-    divCarousel.style.width = width;
-    divCarousel.style.height = height;
+    divCarousel.style.width = width+"px";
+    divCarousel.style.height = height+"px";
     Slider._properties.WidthSlide = width;
   } else {
-    divCarousel.style.width = "inherit";
-    divCarousel.style.height = "inherit";
-    console.log(ElemAppend.offsetWidth);
+    divCarousel.style.width = Slider._properties.StyleCarousel.width;
+    divCarousel.style.height = Slider._properties.StyleCarousel.height;
     Slider._properties.WidthSlide = ElemAppend.offsetWidth;
   }
   divCarousel.style.overflow = "hidden";
 
-  var divCarouselContent = document.createElement("div");
-
+  var divCarouselContent = document.createElement("div"); // контент div для карусели
   divCarouselContent.id = "content";
   divCarouselContent.style.width = "inherit";
   divCarouselContent.style.height = "inherit";
@@ -40,43 +44,58 @@ Sls.prototype.createCarousel = function(ElemAppend, idElem, width = null, height
   // Добовления всего во все =>
   ElemAppend.appendChild(divCarousel);
   divCarousel.appendChild(divCarouselContent);
-
-  // Убрать шлак за собой =>
-  Slider.createSlide(null, {display: "inline-block", position: "relative"});
-  Slider.createSlide(null);
-  Slider.setWidthContent();
 };
 
-// Создание слайда (css код только относящийся к формированию одного слайда)=>
+/**
+ * Sls.prototype.createSlide - Создание нового слайда
+ *
+ * @param  {string} htmlCode = null html код который будет распологаться в слайде
+ * @param  {object} cssCode = null  css код который будет распологаться в слайде
+ */
 Sls.prototype.createSlide = function(htmlCode = null, cssCode = null) {
   var Slide = document.createElement("div");
   Slide.className = "sliders";
-  Slide.style.width = "inherit";
+  Slide.style.width = Slider._properties.WidthSlide+"px";
   Slide.style.height = "inherit";
+  Slide.style.display = "inline-block";
+  Slide.style.position = "relative";
+
+  var ContentInSlide = document.createElement("div");
+  ContentInSlide.style.position = "absolute";
+  ContentInSlide.className = "contentInSlide";
 
   if(cssCode !== null) {
     for(var key in cssCode) {
       if(key != "width" && key != "height")
-        eval("Slide.style."+key+" = "+"'"+cssCode[key]+"'");
+        eval("ContentInSlide.style."+key+" = "+"'"+cssCode[key]+"'");
     }
   }
 
-  htmlCode !== null ? Slide.innerHTML = htmlCode : Slide.innerHTML = "";
+  htmlCode !== null ? ContentInSlide.innerHTML = htmlCode : ContentInSlide.innerHTML = "";
   document.getElementById("content").appendChild(Slide);
+  Slide.appendChild(ContentInSlide);
+  Slider.setWidthContent(); // Перерасчитывает ширину контента
 };
 
-// Считает колл-во слайдов и возвращает колл-во слайдов или колл-во пикселей
+/**
+ * Sls.prototype.checkCountSliders - Считает колл-во слайдов
+ *
+ * @param  {boolean} isCalculated = false Если false то возвращает колл-во слайдов, если true то возвращает просчет пикселей
+ * @return {string}                      возвравщает колл-во слайдов/расчет пикселей
+ */
 Sls.prototype.checkCountSliders = function(isCalculated = false) {
   var countSlider = document.querySelectorAll("#"+Slider._properties.NameCarousel+" .sliders");
   if(!isCalculated)
-    return countSlider;
+    return countSlider.length;
   else
     return countSlider.length*Slider._properties.WidthSlide+"px";
 };
 
+
+/**
+ * Sls.prototype.setWidthContent - Установить новое значение div контента
+ */
 Sls.prototype.setWidthContent = function() {
-  console.log(Slider.checkCountSliders(true));
-  console.log(document.querySelector("#"+Slider._properties.NameCarousel+" #content"));
   document.querySelector("#"+Slider._properties.NameCarousel+" #content").style.width = Slider.checkCountSliders(true);
 }
 
